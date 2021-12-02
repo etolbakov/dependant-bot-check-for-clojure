483fa627b760d1e3060430053bf37ff5486bd210
Eugene Tolbakov <ev.tolbakov@gmail.com>
2021-12-02 15:49:48 +0000

    feat: add some deps

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [metosin/jsonista                   "0.3.0"]
+                 [org.clojure/data.csv               "1.0.0"]
+                 ]
   :repl-options {:init-ns dependant-bot-check-for-clojure.core})

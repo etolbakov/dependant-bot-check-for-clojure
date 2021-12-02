a73fef415a99b14f0af2abbddeb117ec3c720f3c
Eugene Tolbakov <ev.tolbakov@gmail.com>
2021-12-02 15:47:29 +0000

    feat: initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dependant-bot-check-for-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dependant_bot_check_for_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns dependant-bot-check-for-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [dependant-bot-check-for-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

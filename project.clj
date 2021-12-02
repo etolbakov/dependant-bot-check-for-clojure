340dca3d766a907190d190566bdc06a60d01b8ff
Eugene Tolbakov <ev.tolbakov@gmail.com>
2021-12-02 16:20:59 +0000

    feat: revert workflow_dispatch event

--- project.clj
+++ project.clj
@@ -3,8 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :repositories [["test-pr-repo" {:url "https://maven.pkg.github.com/etolbakov/test-pr-repo"
+                                  :username :env/GH_PACKAGES_USR
+                                  :password :env/GH_PACKAGES_PSW}]]
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [metosin/jsonista                   "0.3.0"]
                  [org.clojure/data.csv               "1.0.0"]
+                 [uk.co.hyde-housing/test-pr-repo              "7.1.0"]
                  ]
   :repl-options {:init-ns dependant-bot-check-for-clojure.core})

a2182011e6f1933ec1c86cae17d4856bf3bff858
Eugene Tolbakov <ev.tolbakov@gmail.com>
2021-12-03 14:13:45 +0000

    fix: remove functions out of project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
-
-(defn ver [] (-> "./resources/dependant-bot-check-for-clojure.version" slurp .trim))
-
-(defproject dependant-bot-check-for-clojure #=(ver)
+(defproject dependant-bot-check-for-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
